6760034c2eb7d0ee00af13d7c5f8d95dcef4ead4
Kostis Michailidis <kostismich@gmail.com>
2022-03-23 21:14:12 +0100

    Part 1A: Refactor BetterWander as a subclass of Wander

---
+++ src/main/java/agent/behavior/wander/BetterWander.java
@@ -0,0 +1,36 @@
+package agent.behavior.wander;
+
+import agent.AgentAction;
+import agent.AgentState;
+import environment.CellPerception;
+import environment.Perception;
+
+public class BetterWander extends Wander {
+
+    @Override
+    public void act(AgentState agentState, AgentAction agentAction) {
+
+        if (agentState.hasCarry()) {
+
+            Perception perception = agentState.getPerception();
+
+            for (CellPerception neighbor : perception.getNeighbours()) {
+                if (neighbor != null && neighbor.containsDestination(agentState.getCarry().get().getColor())) {
+                    agentAction.putPacket(neighbor.getX(), neighbor.getY());
+                    return;
+                }
+            }
+        } else {
+            Perception perception = agentState.getPerception();
+
+            for (CellPerception neighbor : perception.getNeighbours()) {
+                if (neighbor != null && neighbor.containsPacket()) {
+                    agentAction.pickPacket(neighbor.getX(), neighbor.getY());
+                    return;
+                }
+            }
+        }
+
+        super.act(agentState, agentAction);
+    }
+}
