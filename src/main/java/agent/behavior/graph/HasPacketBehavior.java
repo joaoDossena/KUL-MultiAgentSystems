a48b3fa7480828710c81c3db4d086c62cf4b956c
joaoDossena <joaopdossena@gmail.com>
2022-03-24 21:16:58 +0100

    Restructuring behaviors

--- src/main/java/agent/behavior/graph/HasPacketBehavior.java
+++ src/main/java/agent/behavior/graph/HasPacketBehavior.java
@@ -4,7 +4,11 @@ import agent.AgentAction;
 import agent.AgentState;
 import agent.behavior.wander.Wander;
 import environment.CellPerception;
+import environment.Coordinate;
 import environment.Perception;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class HasPacketBehavior extends Wander {
 
@@ -12,13 +16,41 @@ public class HasPacketBehavior extends Wander {
     public void act(AgentState agentState, AgentAction agentAction) {
 
         Perception perception = agentState.getPerception();
+        var neighbours = perception.getNeighbours();
 
-        for (CellPerception neighbor : perception.getNeighbours()) {
-            if (neighbor != null && neighbor.containsDestination(agentState.getCarry().get().getColor())) {
+        for(CellPerception neighbor : neighbours){
+            if(neighbor != null && neighbor.containsDestination(agentState.getCarry().get().getColor())){
                 agentAction.putPacket(neighbor.getX(), neighbor.getY());
                 return;
             }
         }
-        super.act(agentState, agentAction);
+        walkTowardsClosestDestination(agentState, agentAction);
+    }
+
+    private void walkTowardsClosestDestination(AgentState agentState, AgentAction agentAction) {
+        Perception perception = agentState.getPerception();
+        List<CellPerception> destinations = perception.getDestinationCells(agentState.getCarry().get().getColor());
+
+        // If no destinations in perception, wander randomly.
+        if(destinations.isEmpty()){
+            super.act(agentState, agentAction);
+            return;
+        }
+
+        // Otherwise, look for closest destination.
+        CellPerception minCell = perception.getClosestCell(destinations, agentState.getX(), agentState.getY());
+
+        // Find the closest walkable move in the direction of minCell
+        List<Coordinate> moves = new ArrayList<>(List.of(
+                new Coordinate(1, 1), new Coordinate(-1, -1),
+                new Coordinate(1, -1), new Coordinate(-1, 1),
+                new Coordinate(1, 0), new Coordinate(-1, 0),
+                new Coordinate(0, 1), new Coordinate(0, -1)
+        ));
+
+        Coordinate minMove = perception.getShortestMoveToCell(minCell, moves, agentState.getX(), agentState.getY());
+
+
+        agentAction.step(agentState.getX() + minMove.getX(), agentState.getY() + minMove.getY());
     }
 }
--- src/main/java/agent/behavior/graph/NoPacketBehavior.java
+++ src/main/java/agent/behavior/graph/NoPacketBehavior.java
@@ -4,7 +4,11 @@ import agent.AgentAction;
 import agent.AgentState;
 import agent.behavior.wander.Wander;
 import environment.CellPerception;
+import environment.Coordinate;
 import environment.Perception;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class NoPacketBehavior extends Wander {
 
@@ -12,13 +16,41 @@ public class NoPacketBehavior extends Wander {
     public void act(AgentState agentState, AgentAction agentAction) {
 
         Perception perception = agentState.getPerception();
+        var neighbours = perception.getNeighbours();
 
-        for (CellPerception neighbor : perception.getNeighbours()) {
-            if (neighbor != null && neighbor.containsPacket()) {
+        for(CellPerception neighbor : neighbours){
+            if(neighbor != null && neighbor.containsPacket()){
                 agentAction.pickPacket(neighbor.getX(), neighbor.getY());
                 return;
             }
         }
-        super.act(agentState, agentAction);
+
+        walkTowardsClosestPacket(agentState, agentAction);
+    }
+
+    private void walkTowardsClosestPacket(AgentState agentState, AgentAction agentAction) {
+        Perception perception = agentState.getPerception();
+        List<CellPerception> packets = perception.getPacketCells();
+
+        // If no packets in perception, wander randomly.
+        if(packets.isEmpty()){
+            super.act(agentState, agentAction);
+            return;
+        }
+
+        // Otherwise, look for closest packet.
+        CellPerception minCell = perception.getClosestCell(packets, agentState.getX(), agentState.getY());
+
+        // Find the closest walkable move in the direction of minCell
+        List<Coordinate> moves = new ArrayList<>(List.of(
+                new Coordinate(1, 1), new Coordinate(-1, -1),
+                new Coordinate(1, -1), new Coordinate(-1, 1),
+                new Coordinate(1, 0), new Coordinate(-1, 0),
+                new Coordinate(0, 1), new Coordinate(0, -1)
+        ));
+
+        Coordinate minMove = perception.getShortestMoveToCell(minCell, moves, agentState.getX(), agentState.getY());
+
+        agentAction.step(agentState.getX() + minMove.getX(), agentState.getY() + minMove.getY());
     }
 }
