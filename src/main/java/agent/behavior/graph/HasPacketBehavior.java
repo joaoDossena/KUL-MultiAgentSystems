ab7f861928f65cf5dfe20a4632abbef486269a70
Kostis Michailidis <kostismich@gmail.com>
2022-03-24 19:10:53 +0100

    Part 1A: Skeleton for graph structured behavior

---
+++ src/main/java/agent/behavior/graph/HasPacketBehavior.java
@@ -0,0 +1,19 @@
+package agent.behavior.graph;
+
+import agent.AgentAction;
+import agent.AgentCommunication;
+import agent.AgentState;
+import agent.behavior.Behavior;
+
+public class HasPacketBehavior extends Behavior {
+
+    @Override
+    public void communicate(AgentState agentState, AgentCommunication agentCommunication) {
+        // No communication
+    }
+
+    @Override
+    public void act(AgentState agentState, AgentAction agentAction) {
+
+    }
+}
