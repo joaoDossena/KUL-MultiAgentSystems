7445b31c8d7fb42977d353e5f6cad5f0b6ca3f9a
Giorgos Patrikis <geopatrikis12@gmail.com>
2022-03-24 12:45:40 +0100

    Works for all games taking into consideration the distance

--- src/main/java/environment/Perception.java
+++ src/main/java/environment/Perception.java
@@ -368,20 +368,20 @@ public class Perception {
             for (int j = 0; j < maxY; j++) {
                 if (getCellAt(i, j) != null) {
                     if (i + 1 >= maxX || getCellAt(i + 1, j) == null) {
-                        shape.add(new LinePoints(new Coordinate(offsetX + i + 1, offsetY + j), 
-                            new Coordinate(offsetX + i + 1, offsetY + j + 1)));
+                        shape.add(new LinePoints(new Coordinate(offsetX + i + 1, offsetY + j),
+                                new Coordinate(offsetX + i + 1, offsetY + j + 1)));
                     }
                     if (i - 1 < 0 || getCellAt(i - 1, j) == null) {
-                        shape.add(new LinePoints(new Coordinate(offsetX + i, offsetY + j), 
-                            new Coordinate(offsetX + i, offsetY + j + 1)));
+                        shape.add(new LinePoints(new Coordinate(offsetX + i, offsetY + j),
+                                new Coordinate(offsetX + i, offsetY + j + 1)));
                     }
                     if (j + 1 >= maxY || getCellAt(i, j + 1) == null) {
-                        shape.add(new LinePoints(new Coordinate(offsetX + i, offsetY + j + 1), 
-                            new Coordinate(offsetX + i + 1, offsetY + j + 1)));
+                        shape.add(new LinePoints(new Coordinate(offsetX + i, offsetY + j + 1),
+                                new Coordinate(offsetX + i + 1, offsetY + j + 1)));
                     }
                     if (j - 1 < 0 || getCellAt(i, j - 1) == null) {
-                        shape.add(new LinePoints(new Coordinate(offsetX + i, offsetY + j), 
-                            new Coordinate(offsetX + i + 1, offsetY + j)));
+                        shape.add(new LinePoints(new Coordinate(offsetX + i, offsetY + j),
+                                new Coordinate(offsetX + i + 1, offsetY + j)));
                     }
                 }
             }
@@ -411,7 +411,7 @@ public class Perception {
         List<CellPerception> cellsWithPackets = new ArrayList<>();
         for(CellPerception[] cellLine : cells){
             for(CellPerception cell : cellLine){
-                if(cell.containsPacket()){
+                if(cell!=null && cell.containsPacket()){
                     cellsWithPackets.add(cell);
                 }
             }
@@ -423,7 +423,7 @@ public class Perception {
         List<CellPerception> cellsWithDestination = new ArrayList<>();
         for(CellPerception[] cellLine : cells){
             for(CellPerception cell : cellLine){
-                if(cell.containsDestination(color)){
+                if(cell!=null && cell.containsDestination(color)){
                     cellsWithDestination.add(cell);
                 }
             }
@@ -447,20 +447,24 @@ public class Perception {
 
     public Coordinate getShortestMoveToCell(CellPerception cell, List<Coordinate> moves, int agentX, int agentY) {
         Coordinate minMove = moves.get(0);
-        int minDistance = manhattanDistance(agentX +  minMove.getX(), agentY + minMove.getY(), cell.getX(), cell.getY());
+        int minDistance = -1;
 
         for (int i = 1; i < moves.size(); i++) {
             Coordinate move = moves.get(i);
             int x = move.getX();
             int y = move.getY();
             int distanceAfterMove = manhattanDistance(agentX + x, agentY + y, cell.getX(), cell.getY());
-            if (getCellPerceptionOnRelPos(x, y) != null && getCellPerceptionOnRelPos(x, y).isWalkable()
-                    && distanceAfterMove < minDistance) {
-                minMove = move;
-                minDistance = distanceAfterMove;
+            if (getCellPerceptionOnRelPos(x, y) != null && getCellPerceptionOnRelPos(x, y).isWalkable())
+            {
+                if(minDistance== -1 || distanceAfterMove<minDistance){
+                    minMove = move;
+                    minDistance = distanceAfterMove;
+                }
+
             }
         }
         return minMove;
     }
 
 }
+
